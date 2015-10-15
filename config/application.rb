9f3029a75dfd9f0487f0544a23f5f5b9a34e6520
Olivier Diguer-St-Amant <oli.diguer@gmail.com>
2015-10-14 23:34:34 -0400

    Background image now local. Added fonts for Lato-Thin. Added the assets/fonts dir to the asset pipeline.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module CassMarieWeb
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #serve crap in the fonts folder as part of the assets pipeline.
+    config.assets.paths << Rails.root.join("app", "assets", "fonts") 
   end
 end
