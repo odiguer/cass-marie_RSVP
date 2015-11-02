7d4c19d302dbeb141ad5b5eddc5fdf6d235e5917
Olivier Diguer <oli.diguer@gmail.com>
2015-11-02 00:33:00 -0500

    Updated Gemfile with Faker gem. Added Seed data to the seed.rb file.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 gem 'bcrypt', '~> 3.1.7'
+gem 'faker', '~> 1.4.2'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+## Users
+User.create!(email: "oli.diguer@foobar.com",
+  first_name:  "Olivier",
+  last_name: "Diguer",
+  password:              "foobar",
+  password_confirmation: "foobar")
+
+10.times do |n|
+  first_name  = Faker::Name.first_name
+  last_name  = Faker::Name.last_name
+  email = "example-#{n+1}@foobar.com"
+  password = "password"
+  User.create!(first_name:  first_name,
+  last_name: last_name,
+  email: email,
+  password:              password,
+  password_confirmation: password)
+end
+
+## Foodstuff
+3.times do |n|
+  Foodstuff.create!(foodstuff_name:  Faker::Lorem.word.titleize, foodstuff_description: Faker::Lorem.sentences.join(' '))
+end
