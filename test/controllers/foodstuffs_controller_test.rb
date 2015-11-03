3a7f40488951b482d9edd064756c37c3977cfbfe
Olivier Diguer <oli.diguer@gmail.com>
2015-11-02 23:01:17 -0500

    Added basic ControllerTests for foodstuff crap.

--- test/controllers/foodstuffs_controller_test.rb
+++ test/controllers/foodstuffs_controller_test.rb
@@ -1,19 +1,33 @@
 require 'test_helper'
 
 class FoodstuffsControllerTest < ActionController::TestCase
-  test "should get list" do
-    get :list
+  def setup
+    @user = users(:olivier)
+    @foodstuff = foodstuffs(:chicken)
+  end
+
+  test "should get index" do
+    log_in_as @user
+    get :index
     assert_response :success
   end
 
-  test "should get load" do
-    get :load
+  test "should get new" do
+    log_in_as @user
+    get :new
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
+  test "should get edit" do
+    log_in_as @user
+    get :edit, id: @foodstuff
     assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get :edit, id: @foodstuff
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
 end
