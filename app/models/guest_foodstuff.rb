b9a9dcfcf983dc0eaf7086f8c8a02c09c891a5c3
Olivier Diguer <oli.diguer@gmail.com>
2015-11-04 21:24:31 -0500

    Removed the guest_foodstuff table migrations

--- app/models/guest_foodstuff.rb
+++
@@ -1,4 +0,0 @@
-class GuestFoodstuff < ActiveRecord::Base
-  belongs_to :guest
-  belongs_to :foodstuff
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031045213) do
+ActiveRecord::Schema.define(version: 20151105020612) do
 
   create_table "foodstuffs", force: :cascade do |t|
     t.string   "foodstuff_name"
@@ -20,23 +20,16 @@ ActiveRecord::Schema.define(version: 20151031045213) do
     t.datetime "updated_at",            null: false
   end
 
-  create_table "guest_foodstuffs", force: :cascade do |t|
-    t.integer  "guest_id"
-    t.integer  "foodstuff_id"
-    t.text     "additional_info"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "guest_foodstuffs", ["foodstuff_id"], name: "index_guest_foodstuffs_on_foodstuff_id"
-  add_index "guest_foodstuffs", ["guest_id"], name: "index_guest_foodstuffs_on_guest_id"
-
   create_table "guests", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "foodstuff_id"
+    t.text     "additional_comment"
   end
+
+  add_index "guests", ["foodstuff_id"], name: "index_guests_on_foodstuff_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
