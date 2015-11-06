e72fb4dd1bbfcf44a34aacc604f5360af5be706c
Olivier Diguer <oli.diguer@gmail.com>
2015-11-06 01:36:24 -0500

    Split the db/seeds file into different files that will be used based on the environment (prod, dev, test)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,31 @@
-## Users
-User.create!(email: "oli.diguer@foobar.com",
-  first_name:  "Olivier",
-  last_name: "Diguer",
-  password:              "foobar",
-  password_confirmation: "foobar")
+load(Rails.root.join( 'db', 'seeds', "#{Rails.env.downcase}.rb"))
 
-10.times do |n|
-  first_name  = Faker::Name.first_name
-  last_name  = Faker::Name.last_name
-  email = "example-#{n+1}@foobar.com"
-  password = "password"
-  User.create!(first_name:  first_name,
-  last_name: last_name,
-  email: email,
-  password:              password,
-  password_confirmation: password)
-end
-
-##Guests
-30.times do |n|
-  full_name  = "#{Faker::Name.first_name} #{Faker::Name.last_name}"
-  Guest.create!(full_name:  full_name)
-end
-
-## Foodstuff
-3.times do |n|
-  Foodstuff.create!(foodstuff_name:  Faker::Lorem.word.titleize, foodstuff_description: Faker::Lorem.sentences.join(' '))
-end
+# ## Users
+# User.create!(email: "oli.diguer@gmail.com",
+#   first_name:  "Olivier",
+#   last_name: "Diguer",
+#   password:              "foobar",
+#   password_confirmation: "foobar")
+#
+# 10.times do |n|
+#   first_name  = Faker::Name.first_name
+#   last_name  = Faker::Name.last_name
+#   email = "example-#{n+1}@foobar.com"
+#   password = "password"
+#   User.create!(first_name:  first_name,
+#   last_name: last_name,
+#   email: email,
+#   password:              password,
+#   password_confirmation: password)
+# end
+#
+# ##Guests
+# 30.times do |n|
+#   full_name  = "#{Faker::Name.first_name} #{Faker::Name.last_name}"
+#   Guest.create!(full_name:  full_name)
+# end
+#
+# ## Foodstuff
+# 3.times do |n|
+#   Foodstuff.create!(foodstuff_name:  Faker::Lorem.word.titleize, foodstuff_description: Faker::Lorem.sentences.join(' '))
+# end
---
+++ db/seeds/production.rb
@@ -0,0 +1,12 @@
+## Users
+User.create!(email: "oli.diguer@gmail.com",
+  first_name:  "Olivier",
+  last_name: "Diguer",
+  password:              "foobar",
+  password_confirmation: "foobar")
+
+User.create!(email: "cassandre.diguer@hotmail.fr",
+  first_name:  "Cassandre",
+  last_name: "Diguer",
+  password:              "foobar",
+  password_confirmation: "foobar")
