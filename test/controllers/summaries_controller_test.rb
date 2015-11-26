5ac1e0ec49cce11ea83abc57594df1a9ab1bf75a
Olivier Diguer <oli.diguer@gmail.com>
2015-11-25 23:06:14 -0500

    Catching up on basic unit tests.

--- test/controllers/summaries_controller_test.rb
+++ test/controllers/summaries_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class SummariesControllerTest < ActionController::TestCase
+  def setup
+    @user = users(:olivier)
+  end
+
   test "should get index" do
+    log_in_as @user
     get :index
     assert_response :success
   end
 
+  test "should redirect when not logged in" do
+    get :index, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,39 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
-  #
-  # test "should get update" do
-  #   get :update
-  #   assert_response :success
-  # end
+  def setup
+    @user = users(:olivier)
+    @user2 = users(:ariane)
+  end
+
+  test "should get index" do
+    log_in_as @user
+    get :index
+    assert_response :success
+  end
+
+  test "should get new" do
+    log_in_as @user
+    get :new
+    assert_response :success
+  end
+
+  test "should get edit" do
+    log_in_as @user
+    get :edit, id: @user
+    assert_response :success
+  end
+
+  test "should redirect edit when wrong user" do
+    log_in_as @user
+    get :edit, id: @user2
+    assert_redirected_to root_url
+  end
+
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 
 end
